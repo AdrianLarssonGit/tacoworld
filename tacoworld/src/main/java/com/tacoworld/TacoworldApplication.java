0d0e6e79d450bc7e42767db82b00961414dd6662
AdrianLarssonGit <developmentgitemail@protonmail.com>
2021-07-06 14:44:05 +0200

    Init commit

---
+++ tacoworld/src/main/java/com/tacoworld/TacoworldApplication.java
@@ -0,0 +1,13 @@
+package com.tacoworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoworldApplication.class, args);
+	}
+
+}
